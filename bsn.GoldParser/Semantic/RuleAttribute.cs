a54777526959a76632387ea364054cf39f712206
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2010-08-04 00:20:45 +0200

    Added documentation

--- bsn.GoldParser/Semantic/RuleAttribute.cs
+++ bsn.GoldParser/Semantic/RuleAttribute.cs
@@ -1,6 +1,22 @@
 using System;
 
 namespace bsn.GoldParser.Semantic {
+	/// <summary>
+	/// <para>Specifies the binding of a specific rule to a constructor (therefore implying the type to be constructed).</para>
+	/// </summary>
+	/// <example>
+	/// <code><![CDATA[
+	/// public class MyRule<T>: SemanticToken {
+	///   [Rule("<X> ::= Y <X>)]
+	///   public MyRule(MyTerminal y, MyRule x) { ... }
+	/// } ]]></code>
+	/// </example>
+	/// <remarks>
+	/// <para>You can apply this attribute to constructors only.</para>
+	/// <para>The class which defines the constructor must inherit from the type used as generic type parameter of the <see cref="SemanticTypeActions{T}"/> and be located in the same assembly in order to be found and associated with the grammar rule.</para>
+	/// <para>Use the <see cref="ConstructorParameterMapping" /> to map rule handles arbitrarily to constructor parameters.</para>
+	/// <para>If the class containing the constructor is generic, you can specify the generic type(s) to use following the rule string.</para>
+	/// </remarks>
 	[AttributeUsage(AttributeTargets.Constructor, AllowMultiple = true, Inherited = false)]
 	public sealed class RuleAttribute: RuleAttributeBase, IEquatable<RuleAttribute> {
 		private readonly Type[] genericTypeParameters;
